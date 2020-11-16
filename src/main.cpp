2521787eb1534f9b5ece8607b093d4928bdcd0cb
seandaniels <sdaniel5@ncsu.edu>
2020-11-16 17:45:40 -0500

    Add base

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "main.h"
+
+#define MODEL 1
+#define INPUT 2
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string inputFileName = argv[INPUT];
+
+    return 0;
+}
