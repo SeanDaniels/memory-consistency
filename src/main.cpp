a7902ad5c642550f0c894ba4d31a030864aa4c9b
seandaniels <sdaniel5@ncsu.edu>
2020-11-16 20:11:13 -0500

    Updated Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <string>
 #include "main.h"
 
-#define MODEL 1
-#define INPUT 2
+#define MODEL 0
+#define INPUT 1
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    string inputFileName = argv[INPUT];
-
+    char* inputFileName = argv[INPUT];
+    printf("%s\n", inputFileName);
     return 0;
 }
